5170e4ea6851796ecc07a12506745ba311dbfd38
MelinaFontao <melinafontaoramos@gmail.com>
2021-06-22 22:45:17 -0300

    Se le agregaron los atributos, getters y setter en las clases iniciales y se agrego un enumerado en la clase Empleada.

--- src/main/java/ar/com/ada/api/empleadas/entities/Categoria.java
+++ src/main/java/ar/com/ada/api/empleadas/entities/Categoria.java
@@ -1,5 +1,39 @@
 package ar.com.ada.api.empleadas.entities;
 
+import java.math.BigDecimal;
+
 public class Categoria {
+
+    private Integer categoria_Id;
+
+    private String nombre;
+
+    private BigDecimal sueldo_Base;
+
+    public Integer getCategoria_Id() {
+        return categoria_Id;
+    }
+
+    public void setCategoria_Id(Integer categoria_Id) {
+        this.categoria_Id = categoria_Id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public BigDecimal getSueldo_Base() {
+        return sueldo_Base;
+    }
+
+    public void setSueldo_Base(BigDecimal sueldo_Base) {
+        this.sueldo_Base = sueldo_Base;
+    }
+
     
+
 }
--- src/main/java/ar/com/ada/api/empleadas/entities/Empleada.java
+++ src/main/java/ar/com/ada/api/empleadas/entities/Empleada.java
@@ -1,5 +1,132 @@
 package ar.com.ada.api.empleadas.entities;
 
+import java.math.BigDecimal;
+import java.sql.Date;
+
 public class Empleada {
+
+    private Integer empleada_Id;
+    private String nombre;
+    private Integer edad;
+    private Categoria categoria;
+    private BigDecimal sueldo;
+    private int estado;
+    private Date fechaAlta;
+    private Date fechaBaja;
+
     
+
+    public Integer getEmpleada_Id() {
+        return empleada_Id;
+    }
+
+    public void setEmpleada_Id(Integer empleada_Id) {
+        this.empleada_Id = empleada_Id;
+    }
+
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+
+
+    public Integer getEdad() {
+        return edad;
+    }
+
+    public void setEdad(Integer edad) {
+        this.edad = edad;
+    }
+
+
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+
+
+    public BigDecimal getSueldo() {
+        return sueldo;
+    }
+
+    public void setSueldo(BigDecimal sueldo) {
+        this.sueldo = sueldo;
+    }
+
+
+
+    //ENUMERADO
+
+    public EstadoEmpleadaEnum getEstado() {
+
+        return EstadoEmpleadaEnum.parse(this.estado);
+    }
+
+    public void setEstadoId(EstadoEmpleadaEnum estado) {
+        this.estado = estado.getValue();
+    }
+
+
+
+    public Date getFechaAlta() {
+        return fechaAlta;
+    }
+
+    public void setFechaAlta(Date fechaAlta) {
+        this.fechaAlta = fechaAlta;
+    }
+
+
+
+    public Date getFechaBaja() {
+        return fechaBaja;
+    }
+
+    public void setFechaBaja(Date fechaBaja) {
+        this.fechaBaja = fechaBaja;
+    }
+
+
+
+    //enumerado
+    public enum EstadoEmpleadaEnum {
+        ACTIVO(1), 
+        INACTIVO(2); 
+        
+
+        private final int value;
+
+        // NOTE: Enum constructor tiene que estar en privado
+        private EstadoEmpleadaEnum(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
+
+        public static EstadoEmpleadaEnum parse(int id) {
+            EstadoEmpleadaEnum status = null; // Default
+            for (EstadoEmpleadaEnum item : EstadoEmpleadaEnum.values()) {
+                if (item.getValue() == id) {
+                    status = item;
+                    break;
+                }
+            }
+            return status;
+        }
+    
+
+    }
 }
