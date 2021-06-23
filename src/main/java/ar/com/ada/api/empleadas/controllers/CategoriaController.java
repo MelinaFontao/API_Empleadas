d055e79952868f7895400134aefbec07c2797afd
MelinaFontao <melinafontaoramos@gmail.com>
2021-06-23 17:21:47 -0300

    Se termina de crear el Service de categoria y el repo de categoria con los metodos base.

--- src/main/java/ar/com/ada/api/empleadas/controllers/CategoriaController.java
+++ src/main/java/ar/com/ada/api/empleadas/controllers/CategoriaController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import ar.com.ada.api.empleadas.entities.*;
 import ar.com.ada.api.empleadas.models.response.GenericResponse;
+import ar.com.ada.api.empleadas.services.CategoriaService;
 
 @RestController
 public class CategoriaController {
